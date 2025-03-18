8a1e44b87b5de3ee999b503b36d6cf2775fc9800
Paul MILLET <paul.millet@lameduse.fr>
2025-03-18 03:42:41 +0100

    fix: hotfix for missing export

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,3 +2,4 @@ export { default as Button } from "./Button";
 export { default as Link } from "./Link";
 export { default as CTA } from "./CTA";
 export { default as Title } from "./Title";
+export { default as Navbar } from "./Navbar";
