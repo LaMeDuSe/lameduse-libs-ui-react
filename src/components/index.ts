32c15587b3ba3d1e9b50e9a1d2cbb81580d6d88f
Paul MILLET <paul.millet@lameduse.fr>
2025-03-20 06:58:17 +0100

    chore: undo revert on ae919ea25acd2381a49eb8650db47f68baabf0c4

--- src/components/index.ts
+++ src/components/index.ts
@@ -4,3 +4,4 @@ export { default as CTA } from "./CTA";
 export { default as Title } from "./Title";
 export { default as Navbar } from "./Navbar";
 export { default as CardOne } from "./CardOne";
+export { default as CardTwo } from "./CardTwo";
