41bf27c47e6cdada846d7c26da3dfcffa162537c
Paul MILLET <paul.millet@lameduse.fr>
2025-04-16 19:55:30 +0200

    feat: added folders

--- src/blocs/Footer/Footer.stories.ts
+++ src/blocs/Footer/Footer.stories.ts
@@ -5,7 +5,7 @@ import Footer from './Footer';
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta = {
-  title: 'LaMeDuSe/Footer',
+  title: 'LaMeDuSe/Blocs/Footer',
   component: Footer,
   parameters: {
     // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
--- src/components/CardOne/CardOne.stories.ts
+++ src/components/CardOne/CardOne.stories.ts
@@ -5,7 +5,7 @@ import CardOne from './CardOne';
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta = {
-  title: 'LaMeDuSe/CardOne',
+  title: 'LaMeDuSe/Components/CardOne',
   component: CardOne,
   parameters: {
     // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
