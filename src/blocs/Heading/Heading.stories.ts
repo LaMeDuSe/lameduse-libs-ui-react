bf96b7bf1c4f5c5b16870e75b6950fbc211599cc
Paul MILLET <paul.millet@lameduse.fr>
2025-04-30 00:29:52 +0200

    feat: added heading

---
+++ src/blocs/Heading/Heading.stories.ts
@@ -0,0 +1,40 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { fn } from '@storybook/test';
+
+import Heading from './Heading';
+
+// More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
+const meta = {
+  title: 'LaMeDuSe/Blocs/Heading',
+  component: Heading,
+  parameters: {
+    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
+    layout: 'fullscreen', // 'fullscreen' | 'padded' | 'centered'
+  },
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
+  tags: ['autodocs'],
+  // More on argTypes: https://storybook.js.org/docs/api/argtypes
+  // argTypes: {
+  //   backgroundColor: { control: 'color' },
+  // },
+  // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
+  args: { 
+    title: 'hello world !',
+    description: 'This is a basic description',
+    image : "https://picsum.photos/2000/300",
+    imagealt: "LaMeDuSe Group Logo",
+   },
+} satisfies Meta<typeof Heading>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+// More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
+export const Primary: Story = {
+  args: {
+    title: 'hello world !',
+    description: 'This is a basic description',
+    image : "https://picsum.photos/2000/300",
+    imagealt: "LaMeDuSe Group Logo",
+  },
+};
