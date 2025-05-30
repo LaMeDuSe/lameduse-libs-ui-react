416d254da68c7c79cbb60e1f2c981ef380ccbf92
Sara Hadouani <s.hadouani@outlook.fr>
2025-05-30 09:40:08 +0200

    feat: modification des fichiers index.ts  own_slider.stories.ts  slider.tsx

--- src/components/own_slider/index.ts
+++ src/components/own_slider/index.ts
@@ -1,4 +1,4 @@
-export * from "./slider"
+export {default} from "./slider"
 
 
 
--- src/components/own_slider/own_slider.stories.ts
+++ src/components/own_slider/own_slider.stories.ts
@@ -1,7 +1,51 @@
-import type { Meta, StoryObj } from '@storybook/react';
-import { fn } from '@storybook/test';
-
-import HeroSlider from './slider';
+import React from 'react';
+import { Meta, StoryObj } from '@storybook/react';
+import { HeroSlider } from './slider';
 import { SliderProvider } from './slideProvider';
+
+const meta: Meta<typeof HeroSlider> = {
+  component: HeroSlider,
+  decorators: [
+    (Story) => (
+      <SliderProvider>
+        <Story />
+      </SliderProvider>
+    ),
+  ],
+};
+
+export default meta;
+
+type Story = StoryObj<typeof HeroSlider>;
+
+const slides = [
+  {
+    image: 'https://dummyimage.com/600x400/002697/ffffff&text=Slide+1',
+    title: 'Slide 1',
+    description: 'Description 1',
+  },
+  {
+    image: 'https://dummyimage.com/600x400/000367/ffffff&text=Slide+2',
+    title: 'Slide 2',
+    description: 'Description 2',
+  },
+  {
+    image: 'https://dummyimage.com/600x400/001133/ffffff&text=Slide+3',
+    title: 'Slide 3',
+    description: 'Description 3',
+  },
+];
+
+export const Default: Story = {
+  args: {
+    slides,
+    width: '100%',
+    height: '500px',
+  },
+};
+
+
+
+
 
 
