af92dcfb39c0d1b8796212155abcd63dc5bf07d0
Sara Hadouani <s.hadouani@outlook.fr>
2025-06-04 18:31:01 +0200

    chore: suppression de fichiers inutiles

--- src/components/own_slider/PaginationProps.ts
+++
@@ -1,5 +0,0 @@
-export interface PaginationProps {
-    total: number; //nombre total de slides
-    activeIndex: number; // index de la slide active
-    onSelect:(index: number)=>void;//fonction pour changer de slide
-}
