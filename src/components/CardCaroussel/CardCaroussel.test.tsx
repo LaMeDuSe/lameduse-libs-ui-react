f7346a5af8b63ce951af8e930041a6b2d33a9fea
Selma Belmehel <selma.belmehel@lameduse.fr>
2025-06-16 15:37:54 +0200

    feat(CardCaroussel): improve layout and fix responsiveness

--- src/components/CardCaroussel/CardCaroussel.test.tsx
+++ src/components/CardCaroussel/CardCaroussel.test.tsx
@@ -1,11 +1,23 @@
 import React from "react";
 import { render } from "@testing-library/react";
-
 import CardCaroussel from "./CardCaroussel";
 
-describe("CardCaroussel", () => {
-  test("renders the card_caroussel component", () => {
-    render(<CardCaroussel yesno="YES" />);
-  });
+beforeAll(() => {
+  global.ResizeObserver = class {
+    observe() {}
+    unobserve() {}
+    disconnect() {}
+  };
 });
 
+describe("CardCaroussel", () => {
+  test("test the CardCaroussel component", () => {
+    const mockCards = [
+      <div key="1">Card 1</div>,
+      <div key="2">Card 2</div>,
+      <div key="3">Card 3</div>,
+    ];
+
+    render(<CardCaroussel Cards={mockCards} />);
+  });
+});
