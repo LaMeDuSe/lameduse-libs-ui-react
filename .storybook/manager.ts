790c85341b77609f2140103afa2b5ab4507d0354
Paul MILLET <paul.millet@lameduse.fr>
2025-06-19 09:52:20 +0200

    fix: button position

--- .storybook/manager.ts
+++ .storybook/manager.ts
@@ -3,8 +3,9 @@ import { addons } from '@storybook/manager-api'
 import { create } from '@storybook/theming'
 
 addons.setConfig({
-   theme: create({
-       base: "light",
-       brandTitle: `My components v.${version}`
+    theme: create({
+        base: "light",
+        brandTitle: `My components v.${version}`,
+        brandImage: `https://assets.lameduse.net/logo/lameduse_logo_grad.webp`
    })
 })
