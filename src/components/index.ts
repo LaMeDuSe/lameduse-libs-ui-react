6662d4c83ddf58fb614c1910fd2c7042a392cafb
Safa AGOURAM <safa.agouram@ensiie.eu>
2025-06-26 15:24:55 +0200

    fix(Chat): add Chat to exports

--- src/components/index.ts
+++ src/components/index.ts
@@ -10,3 +10,4 @@ export { default as Link } from "./Link";
 // Template is not exported as it is not a component
 export { default as Title } from "./Title";
 export { default as Calendar } from "./Calendar"
+export { default as Chat } from "./Chat"
