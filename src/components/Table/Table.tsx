c47eed7d664158b54388c9f305e6ef593b804717
Sara Hadouani <s.hadouani@outlook.fr>
2025-07-03 16:56:49 +0200

    feat: permettre au développeur de définir les données du tableau

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -1,14 +1,31 @@
 import React, { useState } from 'react';
-import Button from '../Button/Button'
-import { ButtonProps } from '../Button/Button'
+import {
+  createColumnHelper,
+  flexRender,
+  getCoreRowModel,
+  useReactTable,
+  ColumnDef,
+} from "@tanstack/react-table";
 
-interface TableProps {
-  buttons?: ButtonProps[],
+
+
+//type Data= string | ButtonProps | File 
+
+/*interface TableProps {
+  data?: Data,
+  columns?: Data[],
   onSearch?: (query: string) => void,
 }
+*/
+
+export type TableProps<T> = {
+  data?: T[];
+  columns?: ColumnDef<T>[];
+  onSearch?: (value: string) => void;
+};
 
 
-function SearchBar({ onSearch }: TableProps) {
+function SearchBar <T,>( props: TableProps<T>) {
   const [query, setQuery] = useState('');
   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const value = event.target.value;
@@ -16,51 +33,69 @@ function SearchBar({ onSearch }: TableProps) {
   };
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    if (onSearch) {
-      onSearch(query);
+    if (props.onSearch && query!="") {
+      props.onSearch(query);
   }};
   return (
+    <div style={{ width: '100%', margin: '0 auto', border: '2px solid grey', borderRadius: '2px' }} >
     <form onSubmit={handleSubmit}>
       <input
       type="search"
       placeholder="Search..."
       onChange={handleInputChange}
-      style={{width: '110px'}}
+      style={{width: '90%'}}
       />
-      <button type="submit">
+      <button style={{float: 'right'}} type="submit" aria-label="Recherche">
         🔍
       </button>
     </form>
-    
+    </div>
   );
 }
 
 
-const Table = ({ buttons, onSearch }: TableProps) => {
+const Table = <T,>({ data, columns, onSearch }: TableProps<T>) => {
+  if (!data || !columns) return null;
+    const table = useReactTable({
+    data,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+  });
   return (
-    <div style={{textAlign: 'center'}}>
-      <div style={{ border: '2px solid black', borderRadius: '2px', transform: 'translateX(-20px)'}}>
+    <div>
         <SearchBar onSearch={onSearch} />
-      </div>
-      <table>
-        <thead>
-          <tr>
-            <th>Actions</th>
+      <br/>      
+      <table style={{width: '100%', border: '2px solid black'}}>
+      <thead>
+        {table.getHeaderGroups().map((headerGroup) => (
+          <tr key={headerGroup.id}>
+            {headerGroup.headers.map((header) => (
+              <th style={{fontWeight: 'bold', padding: '5px'}} key={header.id}>
+                {header.isPlaceholder
+                  ? null
+                  : flexRender(
+                      header.column.columnDef.header,
+                      header.getContext()
+                    )}
+              </th>
+            ))}
           </tr>
-        </thead>
-        <tbody>
-          {buttons?.map((button, index) => (
-            <tr key={index}>
-              <td>
-                <Button {...button} />
+        ))}
+      </thead>
+      <tbody>
+        {table.getRowModel().rows.map((row) => (
+          <tr style={{border: '1px solid black'}} key={row.id}>
+            {row.getVisibleCells().map((cell) => (
+              <td style={{textAlign: 'center', padding: '10px' }} key={cell.id}>
+                {flexRender(cell.column.columnDef.cell, cell.getContext())}
               </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+            ))}
+          </tr>
+        ))}
+      </tbody>
+    </table>
     </div>
-  );
-};
-
+    );
+  }
 export default Table;
 
