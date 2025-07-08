937a7a93001db48c1c94c42976d9e4c48afbfed8
nath256 <56562574+nath256@users.noreply.github.com>
2025-07-08 16:54:44 +0200

    fix: lameduse and identity card icons

--- src/components/TeamCard/TeamCard.tsx
+++ src/components/TeamCard/TeamCard.tsx
@@ -35,12 +35,10 @@ const TeamCard = (props: TeamCardProps) => {
         <Icon icon="LINKEDIN" />
         </Link>}
         {props.url_blog && <Link href={props.url_blog} className="grayscale-50 hover:grayscale-0">
-          <Image src="https://assets.lameduse.net/logo/lameduse_logo_grad.webp" alt="LaMeDuSe blog link" height={24} width={24} className="w-6 h-6" />
+        <Icon icon="LAMEDUSE" />
         </Link>}
         <Link href={`/contacts/${props.link_url}`} className="text-lameduse-primary/80  hover:text-lameduse-primary">
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M15 9h3.75M15 12h3.75M15 15h3.75M4.5 19.5h15a2.25 2.25 0 0 0 2.25-2.25V6.75A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25v10.5A2.25 2.25 0 0 0 4.5 19.5Zm6-10.125a1.875 1.875 0 1 1-3.75 0 1.875 1.875 0 0 1 3.75 0Zm1.294 6.336a6.721 6.721 0 0 1-3.17.789 6.721 6.721 0 0 1-3.168-.789 3.376 3.376 0 0 1 6.338 0Z" />
-          </svg>
+        <Icon icon="IDENTITY CARD" />
         </Link>
       </span>
     </div>
