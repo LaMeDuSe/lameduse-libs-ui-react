dca3c724e02e9dfe0e24b0e3dd19a81507b23201
AZQUA <leyre.ange@gmail.com>
2025-12-18 13:53:49 +0100

    feat: add possibility to use personnal style for image

--- src/blocs/HeroSlider/HeroSlider.tsx
+++ src/blocs/HeroSlider/HeroSlider.tsx
@@ -13,6 +13,7 @@ export interface Slide {
   image: string;
   buttons?: LinkProps[];
   style?: SlideLayout | SlideStyle;
+  imgClassName?: string;
 }
 
 export interface HeroSliderProps {
@@ -74,7 +75,7 @@ const HeroSlider: React.FC<HeroSliderProps> = ({
           <img
             src={slides[currentIndex].image}
             alt={slides[currentIndex].title}
-            className="w-full h-full object-cover"
+            className="w-full h-full object-cover ${slides.imgClassName}"
           />
           <div className={`absolute inset-0 bg-black/40 flex flex-col justify-center text-white px-4 sm:px-20 ${alignmentMap[layout.align]}`}>
             {layout.showTitle && (
