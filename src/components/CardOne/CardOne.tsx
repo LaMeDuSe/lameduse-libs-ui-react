88d3f721f8792521dc9211f92c70ebed8a3fe94e
AZQUA <leyre.ange@gmail.com>
2026-01-08 15:07:32 +0100

    chore: change alignement

--- src/components/CardOne/CardOne.tsx
+++ src/components/CardOne/CardOne.tsx
@@ -49,7 +49,7 @@ const CardOne = (props: CardOneProps) => {
         </div>
         <h2 className="text-2xl font-medium text-gray-900 mt-6 mb-3">{props.title}</h2>
         <p className="leading-relaxed text-base mb-6">{props.description}</p>
-        <div className=" w-fit flex mx-auto mt-auto">
+        <div className=" w-fit flex mx-auto mt-auto b-0">
           <Link href={props.link_url} type={"primary"} form="rounded" style="solid" size="medium" className="border-0 py-2 px-5 focus:outline-none">{props.link_text}</Link>
         </div>
       </div>
