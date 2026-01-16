b217d626c51a80acac800dadc963f61e7021f05c
AZQUA <leyre.ange@gmail.com>
2026-01-16 18:17:27 +0100

    fix: stories

--- src/blocs/Form/Form.stories.ts
+++ src/blocs/Form/Form.stories.ts
@@ -5,11 +5,11 @@ import Form from './Form';
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta = {
-  title: 'LaMeDuSe/Components/Form',
+  title: 'LaMeDuSe/Blocs/Form',
   component: Form,
   parameters: {
     // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
-    layout: 'centered', // 'fullscreen' | 'padded' | 'centered'
+    layout: 'fullscreen', // 'fullscreen' | 'padded' | 'centered'
   },
   // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
   tags: ['autodocs'],
@@ -24,7 +24,7 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 // More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
-export const Primary: Story = {
+export const Default: Story = {
   args: {
     form:[
       {
